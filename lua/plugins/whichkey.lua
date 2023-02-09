fddfadaa54789931c9951970deecbc7183c289f2
Oyinbra Kelvin <35059691+Oyinbra@users.noreply.github.com>
2023-02-09 18:38:59 +0100

    major changes to lsp and lazy plugins

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,104 +1,118 @@
 return {
-    'folke/which-key.nvim',
-    event = "BufWinEnter",
-    config = function()
-      local wk = require("which-key")
-      wk.setup {
-          plugins = {
-              marks = false,
-              registers = false,
-              spelling = { enabled = false, suggestions = 20 },
-              presets = {
-                  operators = false,
-                  motions = false,
-                  text_objects = false,
-                  windows = false,
-                  nav = false,
-                  z = false,
-                  g = false
-              }
-          }
-      }
+	"folke/which-key.nvim",
+	event = "BufWinEnter",
+	config = function()
+		local wk = require("which-key")
+		wk.setup({
+			plugins = {
+				marks = false,
+				registers = false,
+				spelling = { enabled = false, suggestions = 20 },
+				presets = {
+					operators = false,
+					motions = false,
+					text_objects = false,
+					windows = false,
+					nav = false,
+					z = false,
+					g = false,
+				},
+			},
+		})
 
-      local Terminal = require('toggleterm.terminal').Terminal
-      local toggle_float = function()
-        local float = Terminal:new({ direction = "float" })
-        return float:toggle()
-      end
-      local toggle_lazygit = function()
-        local lazygit = Terminal:new({ cmd = 'lazygit', direction = "float" })
-        return lazygit:toggle()
-      end
+		local Terminal = require("toggleterm.terminal").Terminal
+		local toggle_float = function()
+			local float = Terminal:new({ direction = "float" })
+			return float:toggle()
+		end
+		local toggle_lazygit = function()
+			local lazygit = Terminal:new({ cmd = "lazygit", direction = "float" })
+			return lazygit:toggle()
+		end
 
-      local mappings = {
-          r = { ":source %<cr>", "Source file" },
-          f = { ":lua vim.lsp.buf.format()<cr>", "Format" },
-          -- e = { ":Neotree<cr>", "Neotree" },
-          m = { ":Mason<cr>", "Mason" },
-          -- s = {
-          --     name = "Session",
-          --     s = { '<cmd>lua require("persistence").load({ last = true })<cr>', "Last Session" },
-          --     d = { '<cmd>lua require("persistence").load()<cr>', "Last Directory Session" },
-          --     e = { '<cmd>lua require("persistence").stop()<cr>]', "End Last Session" }
-          -- },
-          d = { ":Alpha<cr>", "Dashboard" },
-          q = { name = "Quit", q = { ":q<cr>", "Quit" }, w = { ":q!<cr>", "Quit Without Saving" } },
-          o = { ":ChatGPT<cr>", "Open AI" },
-          w = { name = "Write", w = { ":w<cr>", "Write" }, q = { ":wq<cr>", "Write & Quit" } },
-          x = { ":bdelete<cr>", "Close" },
-          E = { ":e ~/.config/nvim/init.lua<cr>", "Edit config" },
-          [" "] = { ":Telescope find_files<cr>", "Telescope Find Files" },
-          g = { ":Telescope live_grep<cr>", "Telescope Live Grep" },
-          t = {
-              name = "ToggleTerm",
-              t = { ":ToggleTerm<cr>", "Terminal Below" },
-              f = { toggle_float, "Terminal Floating" },
-              l = { toggle_lazygit, "LazyGit" },
-              v = { ":vsplit<cr>", "Vertical Split" },
-              h = { ":split<cr>", "Horizontal Split" }
-          },
-          l = {
-              name = "LSP",
-              l = { ":Lazy<cr>", "Lazy Manager" },
-              i = { ":LspInstall<cr>", "Install" },
-              m = { ":Mason<cr>", "Mason" },
-              j = { ":LspInfo<cr>", "Connected Language Servers" },
-              k = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
-              K = { "<cmd>Lspsaga hover_doc<cr>", "Hover Commands" },
-              w = { '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', "Add Workspace Folder" },
-              W = { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', "Remove Workspace Folder" },
-              u = {
-                  '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',
-                  "List Workspace Folders"
-              },
-              t = { '<cmd>lua vim.lsp.buf.type_definition()<cr>', "Type Definition" },
-              d = { '<cmd>lua vim.lsp.buf.definition()<cr>', "Go To Definition" },
-              D = { '<cmd>lua vim.lsp.buf.declaration()<cr>', "Go To Declaration" },
-              r = { '<cmd>lua vim.lsp.buf.references()<cr>', "References" },
-              R = { '<cmd>Lspsaga rename<cr>', "Rename" },
-              a = { '<cmd>Lspsaga code_action<cr>', "Code Action" },
-              e = { '<cmd>Lspsaga show_line_diagnostics<cr>', "Show Line Diagnostics" },
-              n = { '<cmd>Lspsaga diagnostic_jump_next<cr>', "Go To Next Diagnostic" },
-              N = { '<cmd>Lspsaga diagnostic_jump_prev<cr>', "Go To Previous Diagnostic" }
-          },
-          z = {
-              name = "Focus",
-              z = { ":ZenMode<cr>", "Toggle Zen Mode" },
-              t = { ":Twilight<cr>", "Toggle Twilight" }
-          }
-          -- p = {
-          --   name = "Packer",
-          --   r = {":PackerClean<cr>", "Remove Unused Plugins"},
-          --   c = {":PackerCompile profile=true<cr>", "Recompile Plugins"},
-          --   i = {":PackerInstall<cr>", "Install Plugins"},
-          --   p = {":PackerProfile<cr>", "Packer Profile"},
-          --   s = {":PackerSync<cr>", "Sync Plugins"},
-          --   S = {":PackerStatus<cr>", "Packer Status"},
-          --   u = {":PackerUpdate<cr>", "Update Plugins"}
-          -- }
-      }
+		local mappings = {
+			-- r = { ":source %<cr>", "Source file" },
+			f = { ":lua vim.lsp.buf.format()<cr>", "Format" },
+			-- e = { ":Neotree<cr>", "Neotree" },
+			s = {
+				name = "Session",
+				s = { '<cmd>lua require("persistence").load({ last = true })<cr>', "Last Session" },
+				d = { '<cmd>lua require("persistence").load()<cr>', "Last Directory Session" },
+				e = { '<cmd>lua require("persistence").stop()<cr>]', "End Last Session" },
+			},
+			["r"] = { ":%d+<cr>", "Remove All Text" },
+			["y"] = { ":%y+<cr>", "Yank All Text" },
+			["e"] = { ":NeoTreeReveal<cr>", "Explorer" },
+			p = {
+				name = "Plugins",
+				c = { ":Lazy clean<cr>", "Clean" },
+				C = { ":Lazy check<cr>", "Check" },
+				d = { ":Lazy debug<cr>", "Debug" },
+				i = { ":Lazy install<cr>", "Install" },
+				s = { ":Lazy sync<cr>", "Sync" },
+				l = { ":Lazy log<cr>", "Log" },
+				h = { ":Lazy home<cr>", "Home" },
+				H = { ":Lazy help<cr>", "Help" },
+				p = { ":Lazy profile<cr>", "Profile" },
+				u = { ":Lazy update<cr>", "Update" },
+			},
+			d = { ":Alpha<cr>", "Dashboard" },
+			q = { name = "Quit", q = { ":q<cr>", "Quit" }, w = { ":q!<cr>", "Quit Without Saving" } },
+			o = { ":ChatGPT<cr>", "Open AI" },
+			w = { name = "Write", w = { ":w<cr>", "Write" }, q = { ":wq<cr>", "Write & Quit" } },
+			x = { ":bdelete<cr>", "Close" },
+			E = { ":e ~/.config/nvim/init.lua<cr>", "Edit config" },
+			[" "] = { ":Telescope find_files<cr>", "Telescope Find Files" },
+			g = { ":Telescope live_grep<cr>", "Telescope Live Grep" },
+			t = {
+				name = "Terminal",
+				f = { ":ToggleTerm direction=float<cr>", "Float" },
+				b = { ":ToggleTerm size=10 direction=horizontal<cr>", "Below" },
+				s = { ":ToggleTerm size=50 direction=vertical<cr>", "Side" },
+				v = { ":vsplit<cr>", "Vertical Split" },
+				h = { ":split<cr>", "Horizontal Split" },
+			},
+			l = {
+				name = "LSP",
+				f = { ":Format<cr>", "Format" },
+				a = { ":Lspsaga code_action<cr>", "Code Action" },
+				i = { ":LspInfo<cr>", "Info" },
+				o = { ":Lspsaga outline<cr>", "Code Outline" },
+				I = { ":Lspsaga incoming_calls<cr>", "Incoming Calls" },
+				O = { ":Lspsaga outgoing_calls<cr>", "Outgoing Calls" },
+				m = { ":Mason<cr>", "Mason Installer" },
+				j = {
+					":Lspsaga diagnostic_jump_next<cr>",
+					"Next Diagnostic",
+				},
+				k = {
+					":Lspsaga diagnostic_jump_prev<cr>",
+					"Prev Diagnostic",
+				},
+				r = { ":Lspsaga rename<cr>", "Rename" },
+				d = { ":Telescope diagnostics bufnr=0<cr>", "Document Diagnostics" },
+				w = { ":Telescope diagnostics<cr>", "Workspace Diagnostics" },
+				s = { ":Telescope lsp_document_symbols<cr>", "Document Symbols" },
+				S = { ":Telescope lsp_workspace_symbols<cr>", "Workspace Symbols" },
+			},
+			z = {
+				name = "Focus",
+				z = { ":ZenMode<cr>", "Toggle Zen Mode" },
+				t = { ":Twilight<cr>", "Toggle Twilight" },
+			},
+			-- p = {
+			--   name = "Packer",
+			--   r = {":PackerClean<cr>", "Remove Unused Plugins"},
+			--   c = {":PackerCompile profile=true<cr>", "Recompile Plugins"},
+			--   i = {":PackerInstall<cr>", "Install Plugins"},
+			--   p = {":PackerProfile<cr>", "Packer Profile"},
+			--   s = {":PackerSync<cr>", "Sync Plugins"},
+			--   S = {":PackerStatus<cr>", "Packer Status"},
+			--   u = {":PackerUpdate<cr>", "Update Plugins"}
+			-- }
+		}
 
-      local opts = { prefix = '<leader>' }
-      wk.register(mappings, opts)
-    end
+		local opts = { prefix = "<leader>" }
+		wk.register(mappings, opts)
+	end,
 }
