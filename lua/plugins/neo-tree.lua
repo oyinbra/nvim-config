e128e5105a96b64c2ebda02396909cfc4c595cf3
Oyinbra Kelvin <35059691+Oyinbra@users.noreply.github.com>
2023-02-09 19:32:58 +0100

    close neotree if last windows was closed is now enabled

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -40,7 +40,7 @@ return {
       -- in the form "LspDiagnosticsSignWarning"
 
       require("neo-tree").setup({
-          close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+          close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
           popup_border_style = "rounded",
           enable_git_status = true,
           enable_diagnostics = true,
