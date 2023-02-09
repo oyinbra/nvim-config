029e73442ca671a310ea5f4f3c84fc24a7e78161
Oyinbra Kelvin <35059691+Oyinbra@users.noreply.github.com>
2023-02-09 18:33:29 +0100

    color schemes and themes added

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,73 @@
+return {
+	{
+		"Alexis12119/nightly.nvim",
+		lazy = true,
+	},
+	{
+		"rose-pine/neovim",
+	},
+
+	{
+		"marko-cerovac/material.nvim",
+		lazy = true,
+		config = function()
+			vim.g.material_style = "deep ocean"
+		end,
+	},
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+	},
+
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = true,
+	},
+
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+	},
+
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+	},
+
+	{
+		"rmehri01/onenord.nvim",
+		lazy = true,
+	},
+
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = true,
+	},
+
+	{
+		"navarasu/onedark.nvim",
+		lazy = true,
+	},
+
+	{
+		"tanvirtin/monokai.nvim",
+		lazy = true,
+	},
+
+	{
+		"Mofiqul/dracula.nvim",
+		lazy = true,
+	},
+
+	{
+		"JoosepAlviste/palenightfall.nvim",
+		lazy = true,
+	},
+
+	{
+		"bluz71/vim-nightfly-colors",
+		lazy = true,
+	},
+}
