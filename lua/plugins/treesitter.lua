7a50778ecbcb3c1a6a507aaf4312aab501933faf
Oyinbra Kelvin <35059691+Oyinbra@users.noreply.github.com>
2023-02-13 07:09:18 +0100

    tree sitter options changed from run to build

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
     config = function()
