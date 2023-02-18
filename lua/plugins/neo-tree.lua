6535bff97ecad651ac0958303407f30441b3bcb2
oyinbra <onyinbra@gmail.com>
2023-02-18 08:08:21 +0100

    fixed the unknow requirement warning vy changing it to depndencies and tag to version

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,30 +2,13 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     enabled = true,
     branch = "v2.x",
-    requires = {
+    dependencies = {
         "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim", {
         -- only needed if you want to use the commands with "_with_window_picker" suffix
         's1n7ax/nvim-window-picker',
-        tag = "v1.*",
-        config = function()
-          require 'window-picker'.setup({
-              autoselect_one = true,
-              include_current = false,
-              filter_rules = {
-                  -- filter using buffer options
-                  bo = {
-                      -- if the file type is one of following, the window will be ignored
-                      filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-
-                      -- if the buffer type is one of following, the window will be ignored
-                      buftype = { 'terminal', "quickfix" }
-                  }
-              },
-              other_win_hl_color = '#e35e4f'
-          })
-        end
-    }
+        version = "v1.*",
+    },
     },
     config = function()
       -- Unless you are still migrating, remove the deprecated commands from v1.x
