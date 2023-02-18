2b0f1b201541806c8742291cb504b4cc456c2492
oyinbra <onyinbra@gmail.com>
2023-02-18 08:08:57 +0100

    disabled autopair

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,10 +1,11 @@
 return {
-  'windwp/nvim-autopairs',
-  dependecies = "nvim-cmp",
-  config = function()
-    require('nvim-autopairs').setup({check_ts = true})
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    local cmp = require('cmp')
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+'windwp/nvim-autopairs',
+enabled = false,
+dependecies = "hrsh7th/nvim-cmp",
+config = function()
+require('nvim-autopairs').setup({check_ts = true})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
   end
 }
