b7d5dba47bddcf89715c5222407742f83e2488cf
oyinbra <onyinbra@gmail.com>
2023-02-23 14:55:54 +0100

    reduce neo-tree to 30

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -81,7 +81,7 @@ return {
           },
           window = {
               position = "left",
-              width = 40,
+              width = 30,
               mapping_options = { noremap = true, nowait = true },
               mappings = {
                   ["<space>"] = {
