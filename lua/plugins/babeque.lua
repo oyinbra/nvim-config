07faf76c7153bc4658e27e8335a502bbcd68bc45
oyinbra <onyinbra@gmail.com>
2023-03-01 18:39:00 +0100

    added vscode like winbar

---
+++ lua/plugins/babeque.lua
@@ -0,0 +1,53 @@
+-- ################################################################################
+-- #                                                                              #
+-- #                                 BARBEQUE                                     #
+-- #                       This is a VS Code like winbar                          #
+-- #                                                                              #
+-- ################################################################################
+
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  config = function()
+    require("barbecue").setup({
+      theme = "auto",
+      -- include_buftypes = { "neo-tree" },
+      exclude_filetypes = { "gitcommit", "toggleterm" },
+      show_modified = false,
+      kinds = {
+        File = "", -- File
+        Module = "", -- Module
+        Namespace = "", -- Namespace
+        Package = "", -- Package
+        Class = "", -- Class
+        Method = "", -- Method
+        Property = "", -- Property
+        Field = "", -- Field
+        Constructor = "", -- Constructor
+        Enum = "", -- Enum
+        Interface = "", -- Interface
+        Function = "", -- Function
+        Variable = "", -- Variable
+        Constant = "", -- Constant
+        String = "", -- String
+        Number = "", -- Number
+        Boolean = "◩", -- Boolean
+        Array = "", -- Array
+        Object = "", -- Object
+        Key = "", -- Key
+        Null = "ﳠ", -- Null
+        EnumMember = "", -- EnumMember
+        Struct = "", -- Struct
+        Event = "", -- Event
+        Operator = "", -- Operator
+        TypeParameter = "", -- TypeParameter
+        Macro = "", -- Macro
+      },
+    })
+  end
+}
