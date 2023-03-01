a9afa059a110d5f37c6d6a5ea8058a21ca5ad1fb
oyinbra <onyinbra@gmail.com>
2023-03-01 18:38:36 +0100

    added lsp to bufferline

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,11 +1,30 @@
 return {
-'windwp/nvim-autopairs',
--- enabled = false,
-dependecies = {"hrsh7th/nvim-cmp"},
-config = function()
-require('nvim-autopairs').setup({check_ts = true})
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+  'windwp/nvim-autopairs',
+  -- enabled = false,
+  dependecies = { "hrsh7th/nvim-cmp" },
+  config = function()
+    require('nvim-autopairs').setup({
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        java = false,
+      },
+      disable_filetype = { "TelescopePrompt", "spectre_panel", "vim" },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0, -- Offset from pattern match
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+    })
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
   end
 }
