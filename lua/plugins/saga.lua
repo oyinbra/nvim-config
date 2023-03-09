bfbb43b6a6535881d99e0ad53763ef1f302a190f
oyinbra <onyinbra@gmail.com>
2023-03-09 19:03:07 +0100

    updated version of lspsaga

--- lua/plugins/saga.lua
+++ lua/plugins/saga.lua
@@ -6,41 +6,130 @@
 -- ################################################################################
 
 return {
-  -- 'tami5/lspsaga.nvim',
   "glepnir/lspsaga.nvim",
   -- enabled = false,
   config = function()
-    local lspsaga = require 'lspsaga'
+    local lspsaga = require "lspsaga"
+
     lspsaga.setup {
-      debug = false,
-      use_saga_diagnostic_sign = true,
-      -- diagnostic sign
-      error_sign = "",
-      warn_sign = "",
-      hint_sign = "",
-      infor_sign = "",
-      diagnostic_header_icon = "   ",
-      -- code action title icon
-      code_action_icon = " ",
-      code_action_prompt = { enable = true, sign = true, sign_priority = 40, virtual_text = true },
-      finder_definition_icon = "  ",
-      finder_reference_icon = "  ",
-      max_preview_lines = 10,
-      finder_action_keys = {
-        open = "o",
+      preview = {
+        lines_above = 0,
+        lines_below = 10,
+      },
+
+      code_action = {
+        num_shortcut = true,
+        keys = {
+          quit = ";",
+          exec = "<CR>",
+        },
+      },
+
+      scroll_preview = {
+        scroll_down = "<C-f>",
+        scroll_up = "<C-b>",
+      },
+      request_timeout = 2000,
+
+      lightbulb = {
+        enable = false,
+        enable_in_insert = true,
+        sign = true,
+        sign_priority = 40,
+        virtual_text = true,
+      },
+
+      rename = {
+        quit = ";",
+        exec = "<CR>",
+        in_select = false,
+      },
+
+      finder = {
+        edit = { "o", "<CR>" },
         vsplit = "s",
         split = "i",
-        quit = "q",
-        scroll_down = "<C-f>",
-        scroll_up = "<C-b>"
+        tabe = "t",
+        quit = { ";", "<ESC>" },
       },
-      code_action_keys = { quit = "q", exec = "<CR>" },
-      rename_action_keys = { quit = "<C-c>", exec = "<CR>" },
-      definition_preview_icon = "  ",
-      border_style = "single",
-      rename_prompt_prefix = "➤",
-      server_filetype_map = {},
-      diagnostic_prefix_format = "%d. "
+
+      diagnostic = {
+        insert_winblend = 0,
+        jump_num_shortcut = true,
+        on_insert = false,
+        on_insert_follow = false,
+        show_code_action = true,
+        show_source = true,
+        custom_fix = nil,
+        custom_msg = nil,
+        text_hl_follow = false,
+        border_follow = true,
+        keys = {
+          exec_action = "o",
+          quit = ";",
+        },
+      },
+
+      symbol_in_winbar = {
+        enable = false,
+        separator = "  ",
+        hide_keyword = true,
+        show_file = true,
+        folder_level = 2,
+      },
+
+      definition = {
+        edit = "<C-c>o",
+        vsplit = "<C-c>v",
+        split = "<C-c>i",
+        tabe = "<C-c>t",
+        quit = ";",
+        close = "<Esc>",
+      },
+
+      ui = {
+        theme = "round",
+        border = "rounded",
+        winblend = 0,
+        expand = "",
+        collaspe = "",
+        preview = " ",
+        code_action = "💡",
+        diagnostic = "🐞",
+        incoming = " ",
+        outgoing = " ",
+        hover = " ",
+        kind = {},
+      },
+
+      outline = {
+        win_position = "right",
+        win_with = "",
+        win_width = 30,
+        show_detail = true,
+        auto_preview = true,
+        auto_refresh = true,
+        auto_close = true,
+        custom_sort = nil,
+        keys = {
+          jump = "o",
+          expand_collaspe = "u",
+          quit = ";",
+        },
+      },
+
+      callhierarchy = {
+        show_detail = false,
+        keys = {
+          edit = "e",
+          vsplit = "s",
+          split = "i",
+          tabe = "t",
+          jump = "o",
+          quit = ";",
+          expand_collaspe = "u",
+        },
+      },
     }
   end
 }
