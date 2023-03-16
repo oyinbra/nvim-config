6b21ed4bdfda9a3832b639450a6e374390378dbb
oyinbra <onyinbra@gmail.com>
2023-03-16 15:47:55 +0100

    💻 add background plugin to sync terminal theme with neovim color scheme

---
+++ lua/plugins/background.lua
@@ -0,0 +1,12 @@
+-- ################################################################################
+-- #                                                                              #
+-- #                                BACKGROUND                                    #
+-- #    NOTE: Neovim plugin to sync your terminal theme with your color scheme    #
+-- #                                                                              #
+-- ################################################################################
+
+return {
+  "typicode/bg.nvim",
+  enabled = false,
+  lazy = false,
+}
